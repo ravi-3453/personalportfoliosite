5b2a5875c949bd1b79419e88bdb8fcfa2ce05560
ravi-3453 <ravikumavat123y123@gmail.com>
2023-05-22 15:04:37 +0530

    first commit

---
+++ src/routes/main.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const routes =  express.Router()
+// const jaba = require("../models/contactform")
+const mongoose = require('mongoose');
+const Jaba = mongoose.Schema({
+    name:String,
+    email:String,
+    subject:String,
+    msg:String
+})
+
+const jaba = mongoose.model("review",Jaba)
+
+routes.get("/",(req,res)=>{
+   res.render("index")
+})
+
+routes.get("/downloadpdf",(req,res)=>{
+    res.download("./public/Docs/Resume.pdf")
+})
+
+routes.post("/contactform",async (req,res)=>{
+
+   try {
+   
+       const data = await jaba.create(req.body)
+       console.log("Form submitted");
+       console.log(data);
+       res.redirect("/")
+       
+   } catch (error) {
+       console.log(error);
+   }
+})
+
+
+module.exports = routes;
